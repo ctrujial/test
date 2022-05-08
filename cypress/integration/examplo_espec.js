77c6892185ba303b895710694f94f7fc96ba4100
ctrujial <cris92ta@gmail.com>
2022-05-07 23:50:59 -0500

    selectores

--- cypress/integration/examplo_espec.js
+++ cypress/integration/examplo_espec.js
@@ -1,1 +1,8 @@
 //https://docs.cypress.io/guides/guides/command-line#How-to-run-commands
+
+describe('My First Test', () => {
+    it('Does not do much!', () => {
+      expect(true).to.equal(true)
+    })
+  })
+ 
